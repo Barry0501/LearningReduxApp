0a450dc9f2ecc8e53f5c407c316ce8cdce8f9d8b
Barry <fighttech.dev@gmail.com>
2018-12-08 10:52:01 +0700

    *Fix createStore qua 1 file khác, export các file từ core nằm hết trong file core

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -1,17 +1,3 @@
-import 'package:http/http.dart';
-import 'package:key_value_store/key_value_store.dart';
-import 'package:redux/redux.dart';
-import 'package:core/src/models/app_state.dart';
-import 'src/redux/reducers/app_reducer.dart';
-import 'package:redux_logging/redux_logging.dart';
-
-Store<AppState> createStore(Client client,KeyValueStore keyValueStore){
-  return Store(
-    appReducer,
-    initialState: AppState.initial(),
-    distinct: true,
-    middleware: [
-      LoggingMiddleware.printer()
-    ]
-  );
-}
+export 'src/redux/store.dart';
+export 'src/models/app_state.dart';
+export 'src/redux/actions/common_actions.dart';
