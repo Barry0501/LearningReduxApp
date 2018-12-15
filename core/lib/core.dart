acee1d8d9a2029c29739c2340258ebb8aa1f1456
Barry <fighttech.dev@gmail.com>
2018-12-15 12:26:54 +0700

    [core] + Xử lý AppState

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -3,3 +3,5 @@ export 'src/models/app_state.dart';
 export 'src/redux/actions/common_actions.dart';
 export 'src/i18n/messages.dart';
 export 'src/models/loading_status.dart';
+export 'src/models/show_cache.dart';
+export 'src/models/show.dart';
--- core/lib/src/redux/reducers/app_reducer.dart
+++ core/lib/src/redux/reducers/app_reducer.dart
@@ -1,7 +1,10 @@
 import 'package:core/src/models/app_state.dart';
+import 'package:core/src/redux/show/show_reducer.dart';
+import 'package:core/src/redux/theater/theater_reducer.dart';
 
 AppState appReducer(AppState state,dynamic action){
   return AppState(
-    isLoading: false
+    showState: showReducer(state.showState,action),
+    theaterState: theaterReducer(state.theaterState,action)
   );
 }
