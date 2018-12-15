cdf9a47f3e3c842522820f9da284c0cb13724b22
Barry <fighttech.dev@gmail.com>
2018-12-15 12:22:45 +0700

    [core] + Thêm các class cho common_action

--- core/lib/src/redux/actions/common_actions.dart
+++ core/lib/src/redux/actions/common_actions.dart
@@ -1,1 +1,21 @@
+import 'package:core/src/models/theater.dart';
+
 class InitAction{}
+
+class InitCompleteAction {
+  InitCompleteAction(
+    this.theaters,
+    this.selectedTheater,
+  );
+
+  final List<Theater> theaters;
+  final Theater selectedTheater;
+}
+
+class FetchComingSoonEventsIfNotLoadedAction {}
+
+class ChangeCurrentTheaterAction{
+  ChangeCurrentTheaterAction(this.selectedTheater);
+
+  final Theater selectedTheater;
+}
